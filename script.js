70bc5278057d8dcf8d9bdb5f6ada8ec6e1bd2ba1
SyimykAmanov <ulanbayuuly@gmail.com>
2026-01-30 02:22:23 +0600

    update css normalize

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let keyboard = document.querySelector(".keyboard");
 let display = document.querySelector(".display");
 
@@ -50,61 +49,4 @@ function calculate(num1, num2, operator) {
 	} 
 }
 
-
-
-=======
-let keyboard = document.querySelector(".keyboard");
-let display = document.querySelector(".display");
-
-
-let num1 = "";
-let num2 = "";
-let oper = "";
-let isSecond = false;
-
-keyboard.addEventListener('click', function (event) {
-	if (!event.target.matches('button')) return;
-	display.value = display.value + event.target.textContent;
-
-	if (event.target.dataset.digit) {
-		if (!isSecond) {
-			num1 += event.target.dataset.digit;
-		} else {
-			num2 += event.target.dataset.digit;
-		}
-	}
-	if (event.target.dataset.operator && event.target.dataset.operator !== "equal") {
-		oper = event.target.dataset.operator;
-		isSecond = true;
-	}
-
-	if (event.target.dataset.operator == "equal") {
-		display.value = calculate(Number(num1), Number(num2), oper)
-		num1 = "";
-		num2 = "";
-		isSecond = false;
-	}
-
-	if (event.target.dataset.action) {
-		display.value = "";
-	}
-});
-
-
-
-function calculate(num1, num2, operator) {
-	if (operator == "add") {
-		return num1 + num2;
-	} else if (operator == "subtract") {
-		return num1 - num2;
-	} else if (operator == "multiply") {
-		return num1 * num2;
-	} else if (operator == "divide") {
-		return num1 / num2;
-	} 
-}
-
-
-
->>>>>>> c9cd0e7fb5163ddbc0f44d6375de990c8ca16050
 console.log(calculate(3, 6, "multiply"))
